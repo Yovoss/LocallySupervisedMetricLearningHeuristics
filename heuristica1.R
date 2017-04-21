367dd01593432ff6753070f88bf83ae1d0b4c9ab
Jonathan Medina <jmedina2930@gmail.com>
2017-04-21 14:29:44 -0500

    add dataset

--- heuristica1.R
+++ heuristica1.R
@@ -1,14 +1,22 @@
-input <- read_delim("~/Documentos/MIMICIII/SAPSII.csv",";", escape_double = FALSE, trim_ws = TRUE)
+dataInput <- read.csv(file="~/Documentos/Metaheuristics/LocallySupervisedMetricLearningHeuristics/1_dataSetSAPS2.csv",header = TRUE, sep=",")
+x = dataInput[, c(2:ncol(dataInput))]
+y = dataInput[, c(1)]
 k = 5
-nFeatures = ncol(input)
-pMatrix = matrix(rexp(nFeatures^2, rate=.1),nFeatures)
+detP = 0
+nFeatures = ncol(x)
+while (detP <= 0){
+  pMatrix = matrix(rexp(nFeatures^2, rate=.1),nFeatures)
+  pMatrix = pMatrix %*% t(pMatrix)
+  detP = det(pMatrix)
+}
 
-for (i in 1:nrow(input)){
-  xi = input[1,]
-  for (j in 1:nrow(input)){
+
+for (i in 1:nrow(x)){
+  xi = x[1,]
+  for (j in 1:nrow(x)){
     if (i != j){
-      xj = input[2,]
-      generalMahalanobis[i,j] = t(xi - xj) * pMatrix * (xi-xj) 
+      xj = x[2,]
+      generalMahalanobis[i,j] = t((xi - xj)) %*% pMatrix %*% (xi-xj) 
     }
   }
   #compactNeighborhood = 
